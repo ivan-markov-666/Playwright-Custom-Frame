5291064f50c038d7eddf28de6ec14e8485ad81df
Ivan Markov <IMarkov@objectsystems.com>
2024-06-26 15:27:37 +0300

    Added a new method for reading data from Excel files. Added new example for using that method.

--- tests/weather-underground/wUnderground.spec.ts
+++ tests/weather-underground/wUnderground.spec.ts
@@ -5,6 +5,10 @@ import { test } from "@playwright/test";
 import { BaseClass } from "../../baseClass/baseClass";
 // Import the PO class.
 import PomExample from "../../pom/weather-underground/wUnderground.po";
+// Import the custom method for reading Excel files.
+import { readExcelAsObject } from '../../custom-methods/other-methods/excelFixture';
+// Import path.
+import path from 'path';
 
 //02. Create the "describe" block.
 test.describe("Weather Underground Report", () => {
@@ -21,11 +25,18 @@ test.describe("Weather Underground Report", () => {
     pom = new PomExample(page);
     // Create a new baseClass and include page.
     baseClass = new BaseClass(page);
+    // Excel file path.
+    const filePath = path.resolve('fixtures/excel/test-data.xlsx');
+    // Read data from the Excel file.
+    const excelData = await readExcelAsObject(filePath, 'sheet name');
+    // Accessing all values of a specific key, e.g., "Damage Type"
+    const damageTypes = excelData.map(entry => entry["gender"]);
+    console.log(damageTypes[0]);
   });
 
   //07. Create the "test" block.
   test("Get data from station.", async () => {
     // 1. Navigate to: https://www.wunderground.com/
-    await pom.findStationByLocation(url, "42.4200,-71.1876", "January", "2023");
+    // await pom.findStationByLocation(url, "26.6630,-81.9535", "October", "2022");
   });
 });
